09ee3e67412612435ab54ab9a8d2acf08c0de011
lariSavino <143668009+lariSavino@users.noreply.github.com>
2024-11-19 21:45:23 -0400

    Add files via upload

--- automacao.py
+++ automacao.py
@@ -4,10 +4,28 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 navegador = webdriver.Firefox()
-#teste para adicionar tarefa 
 navegador.get("http://127.0.0.1:5500/projeto_fic/build/index.html")
-navegador.find_element(By.XPATH, '//*[@id="todo-input"]').click() 
-navegador.find_element(By.XPATH, '//*[@id="todo-input"]').send_keys("Estudar javascript")
 
-navegador.find_element(By.XPATH, '//*[@id="data"]').send_keys("19-11-2024T15:30")
-navegador.find_element(By.XPATH, '/html/body/div[1]/form/button').click()
+# Função para adicionar uma tarefa
+def adicionar_tarefa(titulo, data_hora):
+    navegador.find_element(By.XPATH, '//*[@id="todo-input"]').click()
+    navegador.find_element(By.XPATH, '//*[@id="todo-input"]').send_keys(titulo)
+    navegador.find_element(By.XPATH, '//*[@id="data"]').send_keys(data_hora)
+    navegador.find_element(By.XPATH, '/html/body/div[1]/form/button').click()
+    time.sleep(1)  # Esperar um pouco para garantir que a tarefa foi adicionada
+
+# Adicionar tarefas
+adicionar_tarefa("Estudar JavaScript", "19-11-2024T15:30")
+adicionar_tarefa("Revisar Python", "20-11-2024T10:00")
+adicionar_tarefa("Praticar Selenium", "21-11-2024T18:00")
+
+#marcar tarefa como concluída 
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/button[1]').click()
+
+#editar tarefa 
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/button[2]').click()
+campo_nome_update = navegador.find_element(By.XPATH, '//*[@id="nome-update"]')
+campo_nome_update.click()
+campo_nome_update.clear()  # Limpa o campo antes de inserir o novo texto
+campo_nome_update.send_keys("Estudar CSS")
+
